efb9accef34e6e8876bc6d51521dca174ac36a69
rolrodriguez <rolandro@hotmail.com>
2021-03-02 00:21:37 -0400

    Initial commit test

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('TEST');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+})

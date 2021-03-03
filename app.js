1a0a2abbde41d5ce20631e7608685aee74801a1c
rolrodriguez <rolandro@hotmail.com>
2021-03-02 22:42:34 -0400

    Team assignment

--- app.js
+++ app.js
@@ -7,8 +7,56 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.send('TEST');
+app.get('/math', (req, res) => {
+  var op1 = Number(req.query.op1);
+  var op2 = Number(req.query.op2);
+  var oper = req.query.operation;
+  var result = 0;
+  switch (oper) {
+    case "add":
+      result = op1 + op2;
+      break;
+    case "sub":
+      result = op1 - op2;
+      break;
+    case "mul":
+      result = op1 * op2;
+      break;
+    case "div":
+      result = op1 / op2;
+      break;
+    default:
+      break;
+  }
+  // console.log(result);
+  res.render('result', {oper, result});
+});
+
+app.get('/math_service', (req, res) => {
+  var op1 = Number(req.query.op1);
+  var op2 = Number(req.query.op2);
+  var oper = req.query.operation;
+  var result = 0;
+  switch (oper) {
+    case "add":
+      result = op1 + op2;
+      break;
+    case "sub":
+      result = op1 - op2;
+      break;
+    case "mul":
+      result = op1 * op2;
+      break;
+    case "div":
+      result = op1 / op2;
+      break;
+    default:
+      break;
+  }
+  // console.log(result);
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ oper, result }));
+  // res.render('result', {oper, result});
 });
 
 app.listen(PORT, () => {
